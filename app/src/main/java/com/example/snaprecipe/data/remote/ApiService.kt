69a747215796eb387c01efd4775de0d89a3e2b63
Julas <julasmame@gmail.com>
2026-04-30 14:24:53 +0300

    updated to gemini 3.0 flash

--- app/src/main/java/com/example/snaprecipe/data/remote/ApiService.kt
+++ app/src/main/java/com/example/snaprecipe/data/remote/ApiService.kt
@@ -9,7 +9,7 @@ import retrofit2.http.Query
 
 interface ApiService {
 
-    @POST("v1beta/models/gemini-1.5-flash:generateContent")
+    @POST("v1/models/gemini-3.0-flash:generateContent")
     suspend fun analyzeImage(
         @Query("key") apiKey: String,
         @Body body: RequestBody
